58ff7b4786704b21bbee1723144d37817d791e66
negreageorgianalaura <georgiana.laura.negrea@gmail.com>
2020-12-13 11:54:20 +0200

    Add project files.

---
+++ Negrea_Georgiana_Lab10/App.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Negrea_Georgiana_Lab10.Data;
+using System.IO;
+using Xamarin.Forms;
+
+namespace Negrea_Georgiana_Lab10
+{
+    public partial class App : Application
+    {
+        static ShoppingListDatabase database;
+        public static ShoppingListDatabase Database
+        {
+            get
+            {
+                if (database == null)
+                {
+                    database = new
+                   ShoppingListDatabase(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.
+                   LocalApplicationData), "ShoppingList.db3"));
+                }
+                return database;
+            }
+        }
+        public App()
+        {
+            InitializeComponent();
+            MainPage = new NavigationPage(new ListEntryPage());
+        }
+    }
+}
---
+++ Negrea_Georgiana_Lab10/ListEntryPage.xaml.cs
@@ -0,0 +1,35 @@
+﻿using Negrea_Georgiana_Lab10.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Negrea_Georgiana_Lab10
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ListEntryPage : ContentPage
+    {
+        public ListEntryPage()
+        {
+            InitializeComponent();
+        }
+
+        async void OnSaveButtonClicked(object sender, EventArgs e)
+        {
+            var slist = (ShopList)BindingContext;
+            slist.Date = DateTime.UtcNow;
+            await App.Database.SaveShopListAsync(slist);
+            await Navigation.PopAsync();
+        }
+        async void OnDeleteButtonClicked(object sender, EventArgs e)
+        {
+            var slist = (ShopList)BindingContext;
+            await App.Database.DeleteShopListAsync(slist);
+            await Navigation.PopAsync();
+        }
+    }
+}
